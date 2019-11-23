e1240bdf4678f89154c3e39baed60c46c267659f
Niek125 <niek.sleddens@gmail.com>
2019-11-23 19:08:12 +0100

    init commit

---
+++ src/main/java/com/Niek125/projectservice/ProjectServiceApplication.java
@@ -0,0 +1,13 @@
+package com.Niek125.projectservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectServiceApplication.class, args);
+    }
+
+}
